6c34809d047a359563cf8c4d5c7a7359ab362a6f
ArturIshmaev <arturishmaev@yandex.ru>
2023-03-05 18:27:17 +0500

    last commit

--- Tasks_05.py
+++ Tasks_05.py
@@ -7,7 +7,14 @@ task26 = """ Задача 26:  Напишите программу, котора
 A = 3; B = 5 -> 243 (3⁵)
     A = 2; B = 3 -> 8 """
    
-  
+num = int(input("Введите число: "))
+exponenta = int(input("Введите степень: "))
+def exponentional(num, exponenta):
+    if exponenta == 1:
+        return num
+    return num * exponentional(num, exponenta - 1)
+
+print(f"Результат возведения в степень = {exponentional(num, exponenta)}")
    
  
     
@@ -20,3 +27,10 @@ task28 = """ Задача 28: Напишите рекурсивную функц
 
 2 2
     4 """
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+def summa(a, b):
+    if a == 0:
+        return b
+    return summa(a - 1, b + 1)
+print(f" Сумма чисел = {summa(a,b)}")
